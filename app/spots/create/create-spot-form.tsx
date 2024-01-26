17dd30d43b87782d8522190d260e7da70a03685c
kotaro1574 <shake1574@gmail.com>
2024-01-25 16:22:37 -0800

    create: CreateSpotForm

---
+++ app/spots/create/create-spot-form.tsx
@@ -0,0 +1,99 @@
+"use client"
+
+import { useState } from "react"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { Textarea } from "@/components/ui/textarea"
+
+const formSchema = z.object({
+  title: z.string().min(1, { message: "Title is required" }),
+  tricks: z.array(z.string()),
+  description: z.string(),
+})
+
+export default function CreateSpotForm() {
+  const [loading, setLoading] = useState(false)
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: "",
+      tricks: [],
+      description: "",
+    },
+  })
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      setLoading(true)
+    } catch (error) {
+      console.error(error)
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+        <FormField
+          control={form.control}
+          name="title"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>title</FormLabel>
+              <FormControl>
+                <Input {...field} />
+              </FormControl>
+              {form.formState.errors.title && (
+                <FormDescription>
+                  {form.formState.errors.title.message}
+                </FormDescription>
+              )}
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="tricks"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>tricks</FormLabel>
+              <FormControl>
+                <Input {...field} />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="description"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>description</FormLabel>
+              <FormControl>
+                <Textarea {...field} />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+
+        <Button className="block w-full" type="submit">
+          {loading ? "loading.." : "Create"}
+        </Button>
+      </form>
+    </Form>
+  )
+}
