4fb0b222a4e0b85d53b95fb91d27f1d3aa7c379b
kotaro1574 <shake1574@gmail.com>
2024-01-30 14:58:57 -0800

    update: newSpots達を表示

---
+++ app/new-spots-table.tsx
@@ -0,0 +1,32 @@
+"use client"
+
+import { siteConfig } from "@/config/site"
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+
+export function NewSpotsTable() {
+  return (
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead>Spots</TableHead>
+          <TableHead>Selected Tricks</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {siteConfig.dummySpots.map((spot) => (
+          <TableRow key={spot.title}>
+            <TableCell>{spot.title}</TableCell>
+            <TableCell>{spot.tricks}</TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import dynamic from "next/dynamic"
 
-import { siteConfig } from "@/config/site"
+import { Separator } from "@/components/ui/separator"
+
+import { NewSpotsTable } from "./new-spots-table"
 
 const DynamicMap = dynamic(() => import("@/components/map"), {
   loading: () => <p>A map is loading</p>,
@@ -11,12 +13,13 @@ export default function IndexPage() {
   return (
     <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
       <div className="flex max-w-[980px] flex-col items-start gap-2">
-        <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
-          {siteConfig.name}
-        </h1>
-
         <div className="w-full">
           <DynamicMap />
+          <h1 className="mt-4 text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
+            New spots 📍
+          </h1>
+          <Separator className="mt-2" />
+          <NewSpotsTable />
         </div>
       </div>
     </section>
