68239c5b5d63e86487a5ec47978005a848af5f4d
kotaro1574 <shake1574@gmail.com>
2024-01-31 11:24:04 -0800

    update: editページを作成

--- app/spots/[id]/edit/page.tsx
+++ app/spots/[id]/edit/page.tsx
@@ -1,11 +1,15 @@
-export default function SpotPage() {
+import UpdateSpotForm from "./update-spot-form"
+
+export default function SpotEditPage() {
   return (
     <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
       <div className="flex max-w-[980px] flex-col items-start gap-2">
         <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
           Spot
         </h1>
-        <div className="w-full"></div>
+        <div className="w-full">
+          <UpdateSpotForm />
+        </div>
       </div>
     </section>
   )
