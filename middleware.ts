d22b80330416a7892d101cc37ebecdd3e7847fa1
kotaro1574 <shake1574@gmail.com>
2024-02-01 15:57:22 -0800

    create: middleware

---
+++ middleware.ts
@@ -0,0 +1,28 @@
+import { NextResponse, type NextRequest } from "next/server"
+import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs"
+
+export async function middleware(req: NextRequest) {
+  const res = NextResponse.next()
+  const supabase = createMiddlewareClient({ req, res })
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser()
+
+  console.log(req.nextUrl.pathname, "req.nextUrl.pathname")
+  // if user is signed in and the current path is / redirect the user to /account
+  if (user && req.nextUrl.pathname === "/") {
+    return NextResponse.redirect(new URL("/account", req.url))
+  }
+
+  // if user is not signed in and the current path is not / redirect the user to /
+  if (!user && req.nextUrl.pathname !== "/") {
+    return NextResponse.redirect(new URL("/", req.url))
+  }
+
+  return res
+}
+
+export const config = {
+  matcher: ["/", "/account"],
+}
